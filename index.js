b2895b5d21087dcf9bc8dcbad8c9092bfe8c4596
Amogh Kambale <me@amoghk.com>
2022-05-03 15:15:57 -0500

    mostly working, just need to fix cost

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const calc = new FusionCalculator(customPersonaeByArcana);
+const formatCurrency = (num) => {
+  if (num == 0) return '¥ 0';
+  num = num.toString();
+  num = num.split('').reverse().join('')
+  let str = '';
+  for (let i =0; i < num.length; i++) {
+    const char = num[i];
+    str += char;
+    if ((i + 1) % 3 === 0) str += ',';
+  }
 
-// console.log(calc.getAllResultingRecipesFrom(personaMap.Abaddon));
+  return `¥ ${str.split('').reverse('').join('').slice(1)}`;
+}
+
+const calc = new FusionCalculator(customPersonaeByArcana);
 
 const listOfPersonas = Object.keys(personaMap);
 
@@ -13,29 +25,32 @@ listOfPersonas.forEach(personaName => {
   const graphObj = {
     _name: personaName,
     _deps: [],
+    _cost: 0,
+    _totalCost: 0,
+    Cost: formatCurrency(0),
   }
 
   graph[personaName] = graphObj;
 
   for (let key in fusionToRecipes) {
     const recipe = fusionToRecipes[key];
-    
-    const _deps = [recipe.sources[0].name, recipe.sources[1].name];
+
+    const _deps = recipe.sources.map(source => source.name);
     _deps.sort();
+
     const _name = _deps.join(' + ');
 
     graphObj._deps.push(_name);
     graph[_name] = {
       _deps,
       _name,
-      cost: recipe.cost,
+      _cost: recipe.cost,
+      Cost: formatCurrency(recipe.cost),
     };
   }
-
-  // console.log(typeof fusionToRecipes);
 });
 
-const t = new DependenTree('div', { maxDepth: 11 });
+const t = new DependenTree('div#test');
 
 t.addEntities(graph);
 
@@ -44,37 +59,57 @@ const { upstream, downstream } = t;
 // t.setTree('Abaddon');
 
 
+const cloner = (node, parent) => {
+  const obj = {
+    ...node,
+    _deps: [],
+    _totalCost: this._cost + parent._cost,
+    Cost: 0, //formatCurrency(recipe.cost),
+    'Total Cost': formatCurrency(this._totalCost || 0),
+  }
+
+  return obj;
+}
 
 const results = [];
 
-const recurse = (node, target, depth = 4, path = '') => {
+const recurse = (node, target, depth = 4, path = '', parent = node) => {
   path += " => " + node._name;
+
+  const clone = cloner(node, parent);
+
   if (node._name === target) {
-    // console.log(path);
     results.push(path);
-    return;
+    return clone;
   }
 
   if (depth === 0) {
-    return;
+    return false;
   }
 
   node._deps.forEach(dep => {
-    recurse(dep, target, depth - 1, path);
+    const childClone = recurse(dep, target, depth - 1, path, node);
+    if (childClone) {
+      clone._deps.push(childClone);
+    }
   });
 
+  return clone._deps.length ? clone : false; 
 }
 
 
 const a = performance.now();
 
 // starting point, target
-recurse(downstream.Abaddon, 'King Frost', 6);
+const newDownstream = recurse(downstream.Abaddon, 'King Frost');
 const b = performance.now();
 
-// results.forEach(console.log.bind(this))
 
 console.log(b - a, 'milliseconds');
 console.log((b - a) / 1000, 'seconds');
-// console.log(upstream.Arsene._deps)
-// console.log(downstream.Arsene._deps)
+
+
+const tree = new DependenTree('div#tree');
+tree.addEntities([{_name: 'a'}]);
+tree.downstream = { Abaddon: newDownstream };
+tree.setTree('Abaddon', 'downstream');
