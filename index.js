45a4fcf77508988977c5de55d11ffe86825aec0e
Amogh Kambale <me@amoghk.com>
2022-05-03 22:07:10 -0500

    mvp without dlc or game select

--- index.js
+++ index.js
@@ -1,115 +1,213 @@
-const formatCurrency = (num) => {
-  if (num == 0) return '¥ 0';
-  num = num.toString();
-  num = num.split('').reverse().join('')
-  let str = '';
-  for (let i =0; i < num.length; i++) {
-    const char = num[i];
-    str += char;
-    if ((i + 1) % 3 === 0) str += ',';
+class PersonaGraph {
+  constructor(customPersonaeByArcana, personaMap) {
+    this.calc = new FusionCalculator(customPersonaeByArcana);
+    this.listOfPersonas = Object.keys(personaMap);
+    this.depsGraph = {};
+
+    this.listOfPersonas.forEach(this.populateDeps.bind(this));
+    this.populateGraph();
   }
 
-  return `¥ ${str.split('').reverse('').join('').slice(1)}`;
+  // Creates a graph linking persona's together with 
+  // an inbetween node with a 
+  populateDeps(personaName) {
+    const persona = personaMap[personaName]
+    const fusionToRecipes = this.calc.getRecipes(persona);
+  
+    const graphObj = {
+      _name: personaName,
+      _deps: [],
+    }
+  
+    this.depsGraph[personaName] = graphObj;
+  
+    for (let key in fusionToRecipes) {
+      const recipe = fusionToRecipes[key];
+      
+  
+      const _deps = recipe.sources.map(source => source.name);
+      _deps.sort();
+  
+      const _name = _deps.join(' + ');
+  
+      graphObj._deps.push(_name);
+      this.depsGraph[_name] = {
+        _deps,
+        _name,
+      };
+    }
+  }
+
+  populateGraph() {
+    // Using DependenTree for the graph mapping logic not
+    // actually visualizing this graph because it is far
+    // too connected to provide valuable data to the user.
+    // maxDepth cuts off too much and there can be too many 
+    // fusion recipes on any given persona, this makes too
+    // much noise. There is already a lot of noise as is
+    // with the fusion recipes that do lead to the target.
+    this.tree = new DependenTree('div#test');
+    this.tree.addEntities(this.depsGraph);
+    this.upstream = this.tree.upstream;
+    this.downstream = this.tree.downstream;
+    return { upstream: this.upstream, downstream: this.downstream }
+  }
 }
 
-const calc = new FusionCalculator(customPersonaeByArcana);
 
-const listOfPersonas = Object.keys(personaMap);
 
-const graph = {}
-
-listOfPersonas.forEach(personaName => {
-  const persona = personaMap[personaName]
-  const fusionToRecipes = calc.getRecipes(persona);
-
-  const graphObj = {
-    _name: personaName,
-    _deps: [],
-    _cost: 0,
-    _totalCost: 0,
-    Cost: formatCurrency(0),
+class PersonaDFS {
+  constructor (selectorString) {
+    this.graph = new PersonaGraph(customPersonaeByArcana, personaMap);
+    this.upstream = this.graph.upstream;
+    this.downstream = this.graph.downstream;
+    this.selectorString = selectorString;
+    this.numPaths = 0;
   }
 
-  graph[personaName] = graphObj;
+  dfs(startingPersona, targetPersona, numFusions) {
+    this.reset();
 
-  for (let key in fusionToRecipes) {
-    const recipe = fusionToRecipes[key];
+    const depth = numFusions * 2;
+    
+    const startTime = performance.now();
+    this.newDownstream = this.recurse(this.downstream[startingPersona], targetPersona, depth);
+    const endTime = performance.now();
+    this.searchTime = ((endTime - startTime) / 1000).toFixed(2);
 
-    const _deps = recipe.sources.map(source => source.name);
-    _deps.sort();
 
-    const _name = _deps.join(' + ');
+    // This section hacks the DependenTree code and injects a
+    // phony graph of our own creation. DependenTree, thinking
+    // that "downstream" is it's own graph, will create a a tree
+    // based on this given structure. We're not using DependenTree
+    // for exactly for its intended purpose, but that's fine. The
+    // d3 code it abstracts away is plenty useful as is.
+    if (this.newDownstream) {  
+      this.tree = new DependenTree(this.selectorString);
+      this.tree.addEntities([{_name: 'a'}]);
+      this.tree.downstream = { [startingPersona]: this.newDownstream };
+      this.tree.setTree(startingPersona, 'downstream');
+    } else {
+      alert('No fusion paths found. Try increasing the max number of fusions.');
+    }
 
-    graphObj._deps.push(_name);
-    graph[_name] = {
-      _deps,
-      _name,
-      _cost: recipe.cost,
-      Cost: formatCurrency(recipe.cost),
-    };
+    return {
+      time: this.searchTime,
+      numPaths: this.numPaths,
+    }
   }
+
+
+  reset() {
+    this.numPaths = 0;
+    if (!this.tree) return;
+    this.tree.passedContainerEl.innerHTML = '';
+  }
+  
+  cloneNode(node) {
+    return {
+      ...node,
+      _deps: [],
+    }
+  }
+
+  recurse(node, target, depth = 4, path = '') {
+    path += " => " + node._name;
+  
+    const clone = this.cloneNode(node);
+  
+    if (node._name === target) {
+      this.numPaths++;
+      return clone;
+    }
+  
+    if (depth === 0) {
+      return false;
+    }
+  
+    node._deps.forEach(dep => {
+      const childClone = this.recurse(dep, target, depth - 1, path);
+      if (childClone) {
+        clone._deps.push(childClone);
+      }
+    });
+  
+    return clone._deps.length ? clone : false; 
+  }
+
+}
+
+const d = new PersonaDFS('div#tree');
+
+const select1 = document.getElementById('p1');
+const select2 = document.getElementById('p2')
+
+d.graph.listOfPersonas.forEach(p => {
+  const option = document.createElement('option');
+  option.innerText = p;
+  option.value = p;
+  const clone = option.cloneNode();
+  clone.innerText = p;
+
+  select1.append(option);
+  select2.append(clone);
 });
 
-const t = new DependenTree('div#test');
+const p1 = document.querySelector('#p1')
+const p2 = document.querySelector('#p2')
+const numFusions = document.querySelector('#numFusions')
 
-t.addEntities(graph);
+new SlimSelect({
+  select: p1,
+  placeholder: 'Select a Persona you have'
+});
 
-const { upstream, downstream } = t;
 
-// t.setTree('Abaddon');
+new SlimSelect({
+  select: p2,
+  placeholder: 'Select the Persona you want to make'
+});
 
 
-const cloner = (node, parent) => {
-  const obj = {
-    ...node,
-    _deps: [],
-    _totalCost: this._cost + parent._cost,
-    Cost: 0, //formatCurrency(recipe.cost),
-    'Total Cost': formatCurrency(this._totalCost || 0),
+new SlimSelect({
+  select: numFusions,
+  placeholder: 'Max number of fusions'
+});
+
+
+const form = document.querySelector('form');
+
+function toggleDlc () {
+  if (form.style.display === 'none') {
+    form.style.display = 'block';
+  } else {
+    form.style.display = 'none';
   }
-
-  return obj;
 }
 
-const results = [];
-
-const recurse = (node, target, depth = 4, path = '', parent = node) => {
-  path += " => " + node._name;
-
-  const clone = cloner(node, parent);
-
-  if (node._name === target) {
-    results.push(path);
-    return clone;
+function search() {
+  const start = p1.value;
+  const end = p2.value;
+  const depth = numFusions.value;
+  if (!start || !end || !depth) {
+    alert('Fill all the form fields')
+    return;
   }
-
-  if (depth === 0) {
-    return false;
-  }
-
-  node._deps.forEach(dep => {
-    const childClone = recurse(dep, target, depth - 1, path, node);
-    if (childClone) {
-      clone._deps.push(childClone);
-    }
-  });
-
-  return clone._deps.length ? clone : false; 
+  toggleGif();
+  setTimeout(() => { 
+    console.log(d.dfs(start, end, depth));
+    toggleGif();
+  }, 50);
 }
 
+// const mainForm = document.querySelector('form#main');
 
-const a = performance.now();
+const load = document.getElementById('load');
+function toggleGif() {
+  if (load.style.display === 'none') {
+    load.style.display = 'block';
+  } else {
+    load.style.display = 'none';
+  }
+}
 
-// starting point, target
-const newDownstream = recurse(downstream.Abaddon, 'King Frost');
-const b = performance.now();
-
-
-console.log(b - a, 'milliseconds');
-console.log((b - a) / 1000, 'seconds');
-
-
-const tree = new DependenTree('div#tree');
-tree.addEntities([{_name: 'a'}]);
-tree.downstream = { Abaddon: newDownstream };
-tree.setTree('Abaddon', 'downstream');
